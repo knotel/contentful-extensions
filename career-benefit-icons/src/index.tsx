c033ed697d65a6b04ba5fb2459037fdae5684bee
Luke Chastain <lchastain@knotel.com>
2020-11-11 23:23:09 +0100

    Update editor

--- career-benefit-icons/src/index.tsx
+++ career-benefit-icons/src/index.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import {
   TextInput,
-  DisplayText,
   SectionHeading,
   SelectField,
   Option,
-  Typography
+  Typography,
+  Button,
 } from '@contentful/forma-36-react-components';
 import { init, EditorExtensionSDK } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
@@ -17,51 +17,74 @@ interface AppProps {
   sdk: EditorExtensionSDK;
 }
 
-interface AppState {
+interface BenefitIcon {
   title?: string;
   iconName?: string;
 }
 
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
+interface IconEditorProps {
+  benefitIcon: BenefitIcon
+  index: number
+  updateIcon(benefitIcon: BenefitIcon, index: number): void
+}
 
-    this.state = {
-      title: props.sdk.entry.fields.title.getValue(),
-      iconName: props.sdk.entry.fields.iconName.getValue()
-    };
-  }
+function IconEditor({benefitIcon, index, updateIcon}: IconEditorProps): JSX.Element {
+  const [title, setTitle] = React.useState<string>(benefitIcon?.title || '')
+  const [iconName, setIconName] = React.useState<string | undefined>(benefitIcon.iconName || 'umbrellacorp')
 
-  onTitleChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.props.sdk.entry.fields.title.setValue(event.target.value);
-  };
+  React.useEffect(() => {
+    benefitIcon.title = title
+    benefitIcon.iconName = iconName
+    updateIcon(benefitIcon, index)
+  }, [title, iconName, benefitIcon])
 
-  onIconNameChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.props.sdk.entry.fields.iconName.setValue(event.target.value);
-  };
+  return (
+    <Typography>
+    <SectionHeading>Label</SectionHeading>
+    <TextInput onChange={e => setTitle(e.target.value)} value={title} />
+    <SelectField labelText="Icon" required 
+      onChange={e => setIconName((e.target as HTMLSelectElement).value)} value={iconName}>
+      <Option value="umbrellacorp">umbrellacorp</Option>
+      <Option value="pig">pig</Option>
+      <Option value="bodybuilder">bodybuilder</Option>
+      <Option value="tea">tea</Option>
+      <Option value="train">train</Option>
+      <Option value="heartbeat">heartbeat</Option>
+    </SelectField>
+    <Button>Remove</Button>
+  </Typography>
+  )
+}
 
-  render() {
-    return (
-      <div className="f36-margin--l">
-        <Typography>
-          <DisplayText>Benefit Icon</DisplayText>
-          <SectionHeading>Label</SectionHeading>
-          <TextInput onChange={this.onTitleChangeHandler} value={this.state.title} />
-          <SectionHeading>Icon</SectionHeading>
-          <SelectField helpText="Choose an icon..." labelText="Icon" required onChange={this.onIconNameChangeHandler} value={this.state.iconName}>
-            <Option value="umbrellacorp">umbrellacorp</Option>
-            <Option value="pig">pig</Option>
-            <Option value="bodybuilder">bodybuilder</Option>
-            <Option value="tea">tea</Option>
-            <Option value="train">train</Option>
-            <Option value="heartbeat">heartbeat</Option>
-          </SelectField>
-        </Typography>
-      </div>
-    );
-  }
+function App({ sdk }: AppProps): JSX.Element {
+  const icons = React.useRef<BenefitIcon[]>(sdk.entry.fields.benefitIcons.getValue() || [{}])
+  const [, setForceUpdate] = React.useState(new Date())
+
+  React.useEffect(() => {
+
+  }, [])
+
+  const updateIcon = React.useCallback((benefitIcon: BenefitIcon, index: number) => {
+    icons.current[index] = benefitIcon
+    sdk.entry.fields.benefitIcons.setValue(icons.current)
+  }, [])
+
+  return (
+    <div className="f36-margin--l">
+      {icons.current.map((benefitIcon, index) => (
+        <IconEditor key={`icon-${index}`} benefitIcon={benefitIcon} index={index} updateIcon={updateIcon} />
+      ))}
+      <Button onClick={() => {
+        icons.current = [...icons.current, {}]
+        setForceUpdate(new Date())
+      }}>Add Icon</Button>
+    </div>
+  )
 }
 
 init(sdk => {
-  render(<App sdk={sdk as EditorExtensionSDK} />, document.getElementById('root'));
-});
+  const extension = sdk as EditorExtensionSDK
+  console.log("LC!! editor " + Object.keys(extension.parameters.invocation || {}))
+
+  render(<App sdk={extension} />, document.getElementById('root'))
+})
